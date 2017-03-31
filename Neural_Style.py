098ddc9dd1136b146ea01aac45490e538b641bbe
alexis-jacq <alexis.jacq@gmail.com>
2017-03-31 13:30:22 +0100

    pep8 style

--- Neural_Style.py
+++ Neural_Style.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import numpy as np
 
 import torch
@@ -12,40 +14,46 @@ import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 import torchvision.models as models
 
-######## cuda
+# cuda
 
 use_cuda = torch.cuda.is_available()
-dtype =  torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+dtype = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
 
-######### images
+# images
 
-imsize = 200 # desired size of the output image
+imsize = 200  # desired size of the output image
 
 loader = transforms.Compose([
-            transforms.Scale(imsize), # scale imported image
-            transforms.ToTensor()]) # transform it into a torch tensor
+    transforms.Scale(imsize),  # scale imported image
+    transforms.ToTensor()])  # transform it into a torch tensor
+
 
 def image_loader(image_name):
     image = Image.open(image_name)
     image = Variable(loader(image))
-    image = image.unsqueeze(0) # fake batch dimension required to fit network's input dimensions
+    # fake batch dimension required to fit network's input dimensions
+    image = image.unsqueeze(0)
     return image
+
 
 style = image_loader("images/picasso.jpg").type(dtype)
 content = image_loader("images/dancing.jpg").type(dtype)
 
-assert style.size() == content.size(), "we need to import style and content images of the same size"
+assert style.size() == content.size(
+),"we need to import style and content images of the same size"
 
 
-########## display
+# display
 
-unloader = transforms.ToPILImage() # reconvert into PIL image
+unloader = transforms.ToPILImage()  # reconvert into PIL image
+
 
 def imshow(tensor):
-    image = tensor.clone().cpu() # we clone the tensor in order to not do changes on it
-    image = image.view(3,imsize,imsize) # remove the fake batch dimension
+    image = tensor.clone().cpu()  # we clone the tensor to not do changes on it
+    image = image.view(3, imsize, imsize)  # remove the fake batch dimension
     image = unloader(image)
     plt.imshow(image)
+
 
 fig = plt.figure()
 
@@ -54,20 +62,22 @@ imshow(style.data)
 plt.subplot(222)
 imshow(content.data)
 
-########## content loss
+# content loss
+
 
 class ContentLoss(nn.Module):
     def __init__(self, target, weight):
         super(ContentLoss, self).__init__()
-        self.target = target.detach() * weight # we 'detach' the target content from the tree used
-                                                # to dynamically compute the gradient: this is a stated value,
-                                                # not a variable. Otherwise the forward method of the criterion
-                                                # will throw an error.
+        # we 'detach' the target content from the tree used
+        self.target = target.detach() * weight
+        # to dynamically compute the gradient: this is a stated value,
+        # not a variable. Otherwise the forward method of the criterion
+        # will throw an error.
         self.weight = weight
         self.criterion = nn.MSELoss()
 
     def forward(self, input):
-        self.loss = self.criterion.forward(input*self.weight, self.target)
+        self.loss = self.criterion.forward(input * self.weight, self.target)
         self.output = input
         return self.output
 
@@ -75,41 +85,45 @@ class ContentLoss(nn.Module):
         self.loss.backward(retain_variables=retain_variables)
         return self.loss.data[0]
 
-######### style loss
+# style loss
+
 
 class GramMatrix(nn.Module):
     def forward(self, input):
-        a,b,c,d = input.size() # a=batch size(=1)
-                               # b=number of feature maps
-                               # (c,d)=dimensions of a f. map (N=c*d)
+        a, b, c, d = input.size()  # a=batch size(=1)
+        # b=number of feature maps
+        # (c,d)=dimensions of a f. map (N=c*d)
 
-        features = input.view(a*b,c*d) # resise F_XL into \hat F_XL
+        features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
 
-        G = torch.mm(features, features.t()) # compute the gram product
+        G = torch.mm(features, features.t())  # compute the gram product
 
-        return G.div(a*b*c*d) # we 'normalize' the values of the gram matrix
-                           # by dividing by the number of element in each feature maps.
+        # we 'normalize' the values of the gram matrix
+        return G.div(a * b * c * d)
+        # by dividing by the number of element in each feature maps.
+
 
 class StyleLoss(nn.Module):
-   def __init__(self, target, weight):
-       super(StyleLoss, self).__init__()
-       self.target = target.detach()*weight
-       self.weight = weight
-       self.gram = GramMatrix()
-       self.criterion = nn.MSELoss()
+    def __init__(self, target, weight):
+        super(StyleLoss, self).__init__()
+        self.target = target.detach() * weight
+        self.weight = weight
+        self.gram = GramMatrix()
+        self.criterion = nn.MSELoss()
 
-   def forward(self, input):
-       self.output = input.clone()
-       self.G = self.gram.forward(input)
-       self.G.mul_(self.weight)
-       self.loss = self.criterion.forward(self.G, self.target)
-       return self.output
+    def forward(self, input):
+        self.output = input.clone()
+        self.G = self.gram.forward(input)
+        self.G.mul_(self.weight)
+        self.loss = self.criterion.forward(self.G, self.target)
+        return self.output
 
-   def backward(self, retain_variables=True):
-       self.loss.backward(retain_variables=retain_variables)
-       return self.loss.data[0]
+    def backward(self, retain_variables=True):
+        self.loss.backward(retain_variables=retain_variables)
+        return self.loss.data[0]
 
-####### load the cnn and build the model
+# load the cnn and build the model
+
 
 cnn = models.alexnet(pretrained=True).features
 
@@ -118,15 +132,15 @@ if use_cuda:
     cnn = cnn.cuda()
 
 # desired depth layers to compute style/content losses :
-content_layers = ['conv_4']
-style_layers = ['conv_1','conv_2','conv_3','conv_4','conv_5']
+content_layers = ['conv_3']
+style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
 
 # just in order to have an iterable access to or list of content/syle losses
 content_losses = []
 style_losses = []
 
-model = nn.Sequential() # the new Sequential module network
-gram = GramMatrix() # we need a gram module in order to compute style targets
+model = nn.Sequential()  # the new Sequential module network
+gram = GramMatrix()  # we need a gram module in order to compute style targets
 
 # move these modules to the GPU if possible:
 if use_cuda:
@@ -134,20 +148,20 @@ if use_cuda:
     gram = gram.cuda()
 
 # weigth associated with content and style losses
-content_weight = 5
-style_weight = 5000
+content_weight = 1
+style_weight = 100
 
 i = 1
 for layer in list(cnn):
-    if isinstance(layer,nn.Conv2d):
-        name = "conv_"+str(i)
-        model.add_module(name,layer)
+    if isinstance(layer, nn.Conv2d):
+        name = "conv_" + str(i)
+        model.add_module(name, layer)
 
         if name in content_layers:
             # add content loss:
             target = model.forward(content).clone()
             content_loss = ContentLoss(target, content_weight)
-            model.add_module("content_loss_"+str(i),content_loss)
+            model.add_module("content_loss_" + str(i), content_loss)
             content_losses.append(content_loss)
 
         if name in style_layers:
@@ -155,18 +169,18 @@ for layer in list(cnn):
             target_feature = model.forward(style).clone()
             target_feature_gram = gram.forward(target_feature)
             style_loss = StyleLoss(target_feature_gram, style_weight)
-            model.add_module("style_loss_"+str(i),style_loss)
+            model.add_module("style_loss_" + str(i), style_loss)
             style_losses.append(style_loss)
 
-    if isinstance(layer,nn.ReLU):
-        name = "relu_"+str(i)
-        model.add_module(name,layer)
+    if isinstance(layer, nn.ReLU):
+        name = "relu_" + str(i)
+        model.add_module(name, layer)
 
         if name in content_layers:
             # add content loss:
             target = model.forward(content).clone()
             content_loss = ContentLoss(target, content_weight)
-            model.add_module("content_loss_"+str(i),content_loss)
+            model.add_module("content_loss_" + str(i), content_loss)
             content_losses.append(content_loss)
 
         if name in style_layers:
@@ -174,36 +188,37 @@ for layer in list(cnn):
             target_feature = model.forward(style).clone()
             target_feature_gram = gram.forward(target_feature)
             style_loss = StyleLoss(target_feature_gram, style_weight)
-            model.add_module("style_loss_"+str(i),style_loss)
+            model.add_module("style_loss_" + str(i), style_loss)
             style_losses.append(style_loss)
 
-        i+=1
+        i += 1
 
-    if isinstance(layer,nn.MaxPool2d):
-        name = "pool_"+str(i)
-        model.add_module(name,layer) # ***
+    if isinstance(layer, nn.MaxPool2d):
+        name = "pool_" + str(i)
+        model.add_module(name, layer)  # ***
 
-print model
+print(model)
 
-###### input image
+# input image
 
 input = image_loader("images/dancing.jpg").type(dtype)
 # if we want to fill it with a white noise:
-# input.data = torch.randn(input.data.size())
+#input.data = torch.randn(input.data.size()).type(dtype)
 
 # add the original input image to the figure:
 plt.subplot(223)
 imshow(input.data)
 
-######## gradient descent
+# gradient descent
 
-input = nn.Parameter(input.data) # this line to show that input is a parameter that requires a gradient
-optimizer = optim.Adam([input], lr = 0.01)
+# this line to show that input is a parameter that requires a gradient
+input = nn.Parameter(input.data)
+optimizer = optim.Adam([input], lr=0.01)
 
 for run in range(500):
 
     # correct the values of updated input image
-    input.data.clamp_(0,1)
+    input.data.clamp_(0, 1)
 
     optimizer.zero_grad()
     model.forward(input)
@@ -217,13 +232,13 @@ for run in range(500):
 
     optimizer.step()
 
-    if run%10==0:
-        print "run "+str(run)+":"
-        print style_score
-        print content_score
+    if run % 10 == 0:
+        print("run " + str(run) + ":")
+        print(style_score)
+        print(content_score)
 
 # a last correction...
-input.data.clamp_(0,1)
+input.data.clamp_(0, 1)
 
 # finally enjoy the result:
 plt.subplot(224)
